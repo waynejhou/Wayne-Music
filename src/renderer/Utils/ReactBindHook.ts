b8e323bfbbcd5402309767acaa00ecc64eba7bd0
Wayne Jhou <jhouwayne@gmail.com>
2020-02-13 18:40:32 +0800

    fix bug that it will notify unchanged update

--- src/renderer/Utils/ReactBindHook.ts
+++ src/renderer/Utils/ReactBindHook.ts
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react"
-import { INotifyPropChanged } from "../../shared/INotifyPropChanged"
+import { INotifyPropChanged, PropertyChangedEventArgs } from "../../shared/INotifyPropChanged"
 
-export function useBind<T>(propName: string, source:INotifyPropChanged) {
+export function useBind<T>(propName: string, source: INotifyPropChanged) {
     const [prop, setProp] = useState(source[propName] as T)
     useEffect(() => {
-        function update() {
-            setProp(source[propName])
+        function update(sender: any, args: PropertyChangedEventArgs) {
+            if (args.propName == propName) setProp(source[propName])
         }
         source.onPropChanged.do(update)
-        return ()=>{
+        return () => {
             source.onPropChanged.remove(update)
         }
     })
